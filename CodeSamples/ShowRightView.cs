39e9b5f80cb15f0031382329105a63abcfeadd7e
hannahham94 <90972226+hannahham94@users.noreply.github.com>
2021-09-18 09:31:41 -0500

    Add files via upload

---
+++ CodeSamples/ShowRightView.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+				
+public class TreeNode {
+	public TreeNode left = null;
+	public TreeNode right = null;
+	public char id;
+	
+	public TreeNode(char nodeId) {
+		id = nodeId;
+	}
+	
+	public void setNodes(TreeNode leftNode, TreeNode rightNode) {
+		left = leftNode;
+		right = rightNode;
+	}
+}
+
+public class Program
+{
+	public static void Main()
+	{
+		TreeNode nodeA = new TreeNode('a');
+		TreeNode nodeB = new TreeNode('b');
+		TreeNode nodeC = new TreeNode('c');
+		TreeNode nodeD = new TreeNode('d');
+		TreeNode nodeE = new TreeNode('e');
+		TreeNode nodeF = new TreeNode('f');
+		TreeNode nodeG = new TreeNode('g');
+		
+		nodeA.setNodes(nodeB, nodeC);
+		nodeB.setNodes(nodeD, nodeE);
+		nodeC.setNodes(nodeF, null);
+		nodeD.setNodes(nodeG, null);
+		
+		HashSet<int> heightsVisited = new HashSet<int>();
+		Console.WriteLine(nodeA.id);
+		heightsVisited.Add(0);
+		searchNodes(nodeA, heightsVisited, 0);
+	}
+	
+	public static void searchNodes(TreeNode cur, HashSet<int> heightsVisited,  int height) {
+		if(cur == null) {
+			return;
+		}
+		else if(!heightsVisited.Contains(height)) {
+			Console.WriteLine(cur.id);
+			heightsVisited.Add(height);
+		}
+		
+		searchNodes(cur.right, heightsVisited, height + 1);
+		searchNodes(cur.left, heightsVisited, height + 1);
+	}
+}
---
+++ CodeSamples/trianglePath.cs
@@ -0,0 +1,34 @@
+	public static int bfs(int[][] data, int i, int j) {
+		if(i < 0 || j < 0 || i >= data.Length || j >= data[i].Length) {
+			return 0;
+		}
+		else if(i + 1 == data.Length) {
+			return data[i][j];
+		}
+		
+		int curr = data[i][j];
+
+		return Math.Max(curr + bfs(data, i + 1, j + 1), curr + bfs(data, i + 1, j));
+	}
+	
+	public static void Main()
+	{
+		int[][] data = new int[][] { new int[] {75},
+		new int[] {95, 64},
+		new int[] {17, 47, 82},
+		new int[] {18, 35, 87, 10},
+		new int[] {20, 4, 82, 47, 65},
+		new int[] {19, 1, 23, 75, 3, 34},
+		new int[] {88, 2, 77, 73, 7, 63, 67},
+		new int[] {99, 65, 4, 28, 6, 16, 70, 92},
+		new int[] {41, 41, 26, 56, 83, 40, 80, 70, 33},
+		new int[] {41, 48, 72, 33, 47, 32, 37, 16, 94, 29},
+		new int[] {53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14},
+		new int[] {70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57},
+		new int[] {91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48},
+		new int[] {63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31},
+		new int[] {4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23}};
+
+		int result = bfs(data, 0, 0);
+		Console.WriteLine(result.ToString());
+	}
